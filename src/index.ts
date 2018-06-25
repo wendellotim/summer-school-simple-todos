3c25d0b166f04fd64ac78566aea1a6eee8dd944a
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-06-25 15:46:26 +0300

    ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,59 @@
-/*
-const test = "My variable";
+import './main.css';
+import './bootstrap/css/bootstrap.min.css';
 
+class MyApp{
+    ourList: HTMLInputElement;
 
-class MyClass{
-    testing = "123";
-    constructor(testing: string){
-this.testing = testing;
+    tasks: { task: string, time: string, description: string }[];
+    constructor() {
+    //our array of objects
+    this.tasks = [];
+
+    this.ourList = (<HTMLInputElement>document.getElementById('myUL'));
+
+    const subBtton = document.getElementById('box');
+    if (subBtton) {
+      subBtton.addEventListener('click', (event) =>{this.getData(event); });
     }
+  }
+
+// retrieving data from html form to array
+
+ getData(event: Event) {
+  event.preventDefault();
+
+  const task = (<HTMLInputElement>document.getElementById('tas')).value;
+  const time = (<HTMLInputElement>document.getElementById('tt')).value;
+  const description = (<HTMLInputElement>document.getElementById('des')).value;
+  // create object
+  const object = {
+    task,
+    time,
+    description,
+  };
+  this.tasks.push(object);
+
+  const unorderedList = document.createElement('ul');
+  let listItem = document.createElement('li');
+  let textNode = document.createTextNode(this.tasks[this.tasks.length - 1].task);
+  listItem.appendChild(textNode);
+  unorderedList.appendChild(listItem);
+  this.ourList.appendChild(unorderedList);
+
+
+  listItem = document.createElement('li');
+  textNode = document.createTextNode(this.tasks[this.tasks.length - 1].description);
+  listItem.appendChild(textNode);
+  unorderedList.appendChild(listItem);
+  this.ourList.appendChild(unorderedList);
+
+
+  listItem = document.createElement('li');
+  textNode = document.createTextNode(this.tasks[this.tasks.length - 1].time);
+  listItem.appendChild(textNode);
+  unorderedList.appendChild(listItem);
+  this.ourList.appendChild(unorderedList);
+}
 }
 
-const myClass = new MyClass("5");
-*/
+ new MyApp();
