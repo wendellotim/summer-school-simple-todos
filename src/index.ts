8ae26c99333ad5705abe526263ce697b190a9533
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-06-26 15:41:02 +0300

    Tslint

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,77 @@
-import './main.css';
-import './bootstrap/css/bootstrap.min.css';
+import "./main.css";
+import "./bootstrap/css/bootstrap.min.css";
+interface Task {
+    status?: Status;
+    task: string;
+    time: string;
+    description: string;
+}
+type Status = "active" | "pending" | "complete";
 
-class MyApp{
-    ourList: HTMLInputElement;
+class MyApp {
+    ourList: HTMLElement | null;
 
-    tasks: { task: string, time: string, description: string }[];
+    tasks: Task [];
     constructor() {
-    //our array of objects
+    // our array of objects
     this.tasks = [];
+    this.ourList = document.getElementById("myUL");
 
-    this.ourList = (<HTMLInputElement>document.getElementById('myUL'));
-
-    const subBtton = document.getElementById('box');
+    const subBtton = document.getElementById("box");
     if (subBtton) {
-      subBtton.addEventListener('click', (event) =>{this.getData(event); });
+      subBtton.addEventListener("click", this.eventListener);
     }
   }
+    eventListener = (event: Event) => {
+    this.getData(event);
+}
 
 // retrieving data from html form to array
 
- getData(event: Event) {
+    getData(event: Event) {
   event.preventDefault();
 
-  const task = (<HTMLInputElement>document.getElementById('tas')).value;
-  const time = (<HTMLInputElement>document.getElementById('tt')).value;
-  const description = (<HTMLInputElement>document.getElementById('des')).value;
+  const task1 = (document.getElementById("tas") as HTMLInputElement | null);
+  const newDescription = (document.getElementById("des") as HTMLInputElement | null);
+  const newTime = (document.getElementById("tt") as HTMLInputElement | null);
+  if (task1 && newDescription && newTime) {
+      const task = task1.value;
+      const time = newTime.value;
+      const description = newDescription.value;
   // create object
-  const object = {
+      const object = {
     task,
     time,
-    description,
+    description
   };
-  this.tasks.push(object);
+      this.tasks.push(object);
+  }
 
-  const unorderedList = document.createElement('ul');
-  let listItem = document.createElement('li');
+  const unorderedList = document.createElement("ul");
+  let listItem = document.createElement("li");
   let textNode = document.createTextNode(this.tasks[this.tasks.length - 1].task);
   listItem.appendChild(textNode);
   unorderedList.appendChild(listItem);
+  if (this.ourList) {
   this.ourList.appendChild(unorderedList);
-
-
-  listItem = document.createElement('li');
+  }
+  listItem = document.createElement("li");
   textNode = document.createTextNode(this.tasks[this.tasks.length - 1].description);
   listItem.appendChild(textNode);
   unorderedList.appendChild(listItem);
+  if (this.ourList) {
   this.ourList.appendChild(unorderedList);
+  }
 
-
-  listItem = document.createElement('li');
+  listItem = document.createElement("li");
   textNode = document.createTextNode(this.tasks[this.tasks.length - 1].time);
   listItem.appendChild(textNode);
   unorderedList.appendChild(listItem);
+  if (this.ourList) {
   this.ourList.appendChild(unorderedList);
-}
+  }
+ }
 }
 
- new MyApp();
+// tslint:disable-next-line:no-unused-expression
+new MyApp();
